ca3c02e4ae9510bd52c1a51b8e57d4f72e6bdb4a
Rajapolah <105721082+17mei2022@users.noreply.github.com>
2022-09-26 22:55:36 +0700

    Update set_lol.sh

--- set_lol.sh
+++ set_lol.sh
@@ -1,1 +1,1 @@
-./lolMiner -a ETCHASH --pool etc.2miners.com:11010 --tls on --user 0xbd4326d2a7dd234d487cf7178f03e2ae84c26ce9 --worker Kaum --shortstats 30 --timeprint on --log on --ethstratum ETHPROXY
+./lolMiner -a etchash -o stratum+ssl://etcssl-euro.f2pool.com:8180 -u Kaum22.Kaum
