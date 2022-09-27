f69618dae794b4dabcfd499b88abf20b523b13d7
Rajapolah <105721082+17mei2022@users.noreply.github.com>
2022-09-27 08:14:00 +0700

    Update min_flex.sh

--- min_flex.sh
+++ min_flex.sh
@@ -1,1 +1,1 @@
-./miniZ --stratum+ssl://kaum22.Kidul@etcssl-euro.f2pool.com:8180 --log --extra --latency --mt-auto --list-modes --mode=99
+./miniZ -a etchash -o stratum+ssl://etcssl-euro.f2pool.com:8180 -u kaum22.Kidul --log --extra --latency --mt-auto --list-modes --mode=99
